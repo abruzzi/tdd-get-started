de8de3dbd04e10f5b9324aeddb5fd8d100596805
Juntao Qiu <juntao.qiu@gmail.com>
2018-07-23 20:24:13 +0800

    extract the constant out

---
+++ src/constants.js
@@ -0,0 +1,23 @@
+export const dict = {
+  'd': {
+    status: 'Dev',
+    effort: 0.5
+  },
+  'D': {
+    status: 'Dev',
+    effort: 1.0
+  },
+  'q': {
+    status: 'QA',
+    effort: 0.5
+  },
+  'Q': {
+    status: 'QA',
+    effort: 1.0
+  }
+}
+
+export const placeholder = {
+  status: 'Unknown',
+  effort: 0.0
+}
--- src/translator.js
+++ src/translator.js
@@ -1,37 +1,13 @@
+import {dict, placeholder} from './constants'
+
 const convert = (c) => {
-  switch (c) {
-    case 'd':
-      return {
-        status: 'Dev',
-        effort: 0.5
-      }
-    case 'D':
-      return {
-        status: 'Dev',
-        effort: 1.0
-      }
-    case 'q':
-      return {
-        status: 'QA',
-        effort: 0.5
-      }
-    case 'Q':
-      return {
-        status: 'QA',
-        effort: 1.0
-      }
-    default:
-      return {
-        status: 'Unknown',
-        effort: 0.0
-      }
-  }
+  return dict[c] || placeholder
 }
 
 const translate = (input) => {
   const items = input.split('')
   return items.reduce((accumulator, current) => {
-    const {status, effort} = convert(current)
+    const { status, effort } = convert(current)
     accumulator[status] = (accumulator[status] || 0) + effort
     return accumulator
   }, {})
