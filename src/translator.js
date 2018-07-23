2801fb92072a37c493a914e5accbcec6414cbf49
Juntao Qiu <juntao.qiu@gmail.com>
2018-07-23 19:49:15 +0800

    parse and calc

--- src/translator.js
+++ src/translator.js
@@ -1,3 +1,10 @@
-const translate = () => 0.5
+const translate = (input) => {
+	const items = input.split('')
+	let sum = 0
+	items.forEach((item) => {
+		sum += (item === 'd') ? 0.5 : 1.0
+	})
+	return sum
+}
 
 export {translate}
--- src/translator.test.js
+++ src/translator.test.js
@@ -4,4 +4,12 @@ describe('translator', () => {
 	it('translate d to half dev day', () => {
 		expect(translate('d')).toEqual(0.5)
 	})
+
+	it('translate D to one dev day', () => {
+		expect(translate('D')).toEqual(1.0)
+	})
+
+	it('translate dd to one dev day', () => {
+		expect(translate('dD')).toEqual(1.5)
+	})
 })
